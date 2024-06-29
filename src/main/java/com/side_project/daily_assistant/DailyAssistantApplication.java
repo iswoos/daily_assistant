8a8b6e499809b0780f4a12dfafc4825f92298ede
이승우 <swlee@joara.com>
2024-06-29 19:18:13 +0900

    Begin : 헥사고날 아키텍처로 세팅중

--- src/main/java/com/side_project/daily_assistant/DailyAssistantApplication.java
+++ src/main/java/com/side_project/daily_assistant/DailyAssistantApplication.java
@@ -2,8 +2,10 @@ package com.side_project.daily_assistant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DailyAssistantApplication {
 
 	public static void main(String[] args) {
