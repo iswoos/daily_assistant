b9447e581f27f1057057ef3b8ad3abc0529a2ce9
이승우 <swlee@joara.com>
2024-09-08 18:33:50 +0900

    Fix : Member 관련 컨트롤러 관리 포인트 개별적으로 분리 (별도 커스텀하기 편하게 + 엄격하게 분리하여 관리하기 위함)

--- src/main/java/com/side_project/daily_assistant/adapter/in/member/LoginMemberController.java
+++ src/main/java/com/side_project/daily_assistant/adapter/in/member/LoginMemberController.java
@@ -1,10 +1,8 @@
 package com.side_project.daily_assistant.adapter.in.member;
 
 import com.side_project.daily_assistant.application.port.in.member.LoginMemberUseCase;
-import com.side_project.daily_assistant.application.port.in.member.RegisterMemberUseCase;
 import com.side_project.daily_assistant.dto.global.response.ApiResponse;
 import com.side_project.daily_assistant.dto.requestdto.member.LoginMemberReq;
-import com.side_project.daily_assistant.dto.requestdto.member.RegisterMemberReq;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,17 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class MemberController {
+public class LoginMemberController {
 
-    private final RegisterMemberUseCase registerMemberUseCase;
     private final LoginMemberUseCase loginMemberUseCase;
-
-    // 회원가입
-    @PostMapping("/members/signup")
-    public ResponseEntity<ApiResponse<String>> registerMember(@RequestBody RegisterMemberReq registerMember) {
-        ApiResponse<String> response = ApiResponse.ok(registerMemberUseCase.registerMember(registerMember));
-        return ApiResponse.toResponseEntity(response);
-    }
 
     // 로그인
     @PostMapping("/members/login")
